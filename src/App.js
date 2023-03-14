66e710e3dfe8a80dac811aea3c96f5f5b7ea6cb8
s-saito <s.s.saito108@gmail.com>
2023-03-15 00:56:49 +0900

    TaskコンポーネントにChakraUIを反映

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Task from "./component/Task";
+import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <h1>これはテストです</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
-    </>
+    <Box mt="64px">
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            <Task name="買い物" />
+            <Task name="ランニング" />
+            <Task name="プログラミングの勉強" />
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
 };
 
